47c06e7b0e5b478b0936d8927bd9fed40d5d4b61
it24100201 <it24100201@my.sliit.lk>
2025-09-16 21:46:24 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100201.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\hasindu\\Desktop\\IT24100201")
+
+#Q1.
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+#Q2.
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+#Q3.
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+#Q4.
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
